a81068b727e7173f9f08206658c4e6245471b023
Jeonggyu Park <goodsogi@gmail.com>
2020-08-05 18:08:05 +0900

    main panel 레이아웃 구현중...

--- PlusPedestrianNaviIOS/Panels/MainPanel.swift
+++ PlusPedestrianNaviIOS/Panels/MainPanel.swift
@@ -12,5 +12,29 @@ import UIKit
 class MainPanel: UIViewController, Panelable {
    
     @IBOutlet weak var headerHeight: NSLayoutConstraint!
+    
     @IBOutlet weak var headerPanel: UIView!
+    
+    @IBOutlet weak var handle: UIImageView!
+    
+   
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        makeLayout()
+    }
+    
+    func makeLayout() {
+        let handleBackgroundImg = ImageMaker.getRoundRectangle(width: 46, height: 6, colorHexString: "#69696d", cornerRadius: 3.0, alpha: 1)
+        
+        handle.backgroundColor = UIColor(patternImage: handleBackgroundImg)
+        
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        
+       
+        let headerPanelBackgroundImg = ImageMaker.getRoundRectangleByCorners(width: screenWidth, height: 320, colorHexString: "#333536", byRoundingCorners: [UIRectCorner.topLeft , UIRectCorner.topRight], cornerRadii: 10.0, alpha: 1)
+              
+              headerPanel.backgroundColor = UIColor(patternImage: headerPanelBackgroundImg)
+    }
 }
